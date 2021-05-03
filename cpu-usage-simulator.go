1d000be7871a5d78581de1799a379183acc72903
p76081158 <p76081158@gs.ncku.edu.tw>
2021-05-03 17:19:26 +0800

    Initial commit

---
+++ cpu-usage-simulator.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	input_cmd := "stress-ng -c 1 -l 3 -t 5"
+	cmd := exec.Command("/bin/bash", "-c", input_cmd)
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Got error: %s\n", err.Error())
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9090", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/p76081158/cpu-usage-simulator
+
+go 1.14
