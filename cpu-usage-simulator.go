5fd684aa0a0622b5c49a29346b555904f48ef32b
p76081158 <p76081158@gs.ncku.edu.tw>
2021-05-04 16:00:32 +0800

    fix for alpine

--- cpu-usage-simulator.go
+++ cpu-usage-simulator.go
@@ -8,7 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	input_cmd := "stress-ng -c 1 -l 3 -t 5"
-	cmd := exec.Command("/bin/bash", "-c", input_cmd)
+	cmd := exec.Command("/bin/sh", "-c", input_cmd)
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Got error: %s\n", err.Error())
 	}
