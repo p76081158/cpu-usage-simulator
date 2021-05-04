01f12e45cceea90dfe98d8a0b9cc558226101543
p76081158 <p76081158@gs.ncku.edu.tw>
2021-05-04 16:49:05 +0800

    fix bug

--- cpu-usage-simulator.go
+++ cpu-usage-simulator.go
@@ -7,12 +7,12 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, r.URL.Path)
 	input_cmd := "nohup stress-ng -c 1 -l 3 -t 5"
 	cmd := exec.Command("/bin/sh", "-c", input_cmd)
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Got error: %s\n", err.Error())
 	}
-	fmt.Fprint(w, r.URL.Path)
 }
 
 func main() {
