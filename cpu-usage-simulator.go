55abf7123c71944f5787ca22c07bc99437ce33bd
Vcx <p76081158@gs.ncku.edu.tw>
2021-08-24 14:47:38 +0800

    fix typesetting and add comment

--- cpu-usage-simulator.go
+++ cpu-usage-simulator.go
@@ -7,14 +7,16 @@ import (
 	"net/http"
 )
 
-var cpu = "1"
+// stress environment variable of service
+var cpu  = "1"
 var load = "3"
 var time = "5"
 
+// handle http requests
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, r.URL.Path)
 	input_cmd := "stress-ng -c " + cpu + " -l " + load + " -t " + time
-	cmd := exec.Command("/bin/sh", "-c", input_cmd)
+	cmd       := exec.Command("/bin/sh", "-c", input_cmd)
 	if err := cmd.Run(); err != nil {
 		fmt.Printf("Got error: %s\n", err.Error())
 	}
@@ -30,6 +32,8 @@ func main() {
 	if (os.Args[4]!="") {
 		time = string(os.Args[4])
 	}
+
+	// simulate service listen on port 9090
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":9090", nil)
 }
